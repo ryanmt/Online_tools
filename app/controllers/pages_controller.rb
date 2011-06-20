f5d9143807589bb52d0db0840e73fd2ef453c130
Ryan Taylor <ryanmt@byu.net>
2011-06-20 16:19:22 -0600

    working on the dead_volume calculating page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,14 @@
 class PagesController < ApplicationController
 
   def index
-
   end
-
-
+  
+  def dead_volume
+    @object = Object.new
+  end
+  
+  def calculate(hash)
+    hash.inspect
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 OnlineTools::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
-
+  get "pages/dead_volume"
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
@@ -48,7 +48,7 @@ OnlineTools::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "pages#index"
 
   # See how all your routes lay out with "rake routes"
 
