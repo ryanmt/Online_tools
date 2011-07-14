0df2e0a0b90f1566c87b38eb69d427b7ba9d46e9
Ryan Taylor <ryanmt@byu.net>
2011-07-14 12:54:10 -0600

    well, it magically works, thanks Jamison!

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,21 @@
 class PagesController < ApplicationController
+  respond_to :json, :only => [:calculate]
 
   def index
   end
   
   def dead_volume
-    @object = Object.new
-  end
-  
-  def calculate
-    @object = params.inspect
-    
+    radius = params[:diameter].to_f/2.0
+    diameter_unit = params[:diameter_unit]
+    length = params[:length].to_f
+    length_unit = params[:length_unit]
+    length = length / 39.3700787 if length_unit != 'meter'
+    radius = radius / 3.93700787e-5 if diameter_unit != 'micrometer'
+    @answer = (radius**2) * Math::PI * length * 1e-3
+    puts "@answer = #{@answer}"
+    respond_to do |format|
+      format.js 
+      format.html
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ OnlineTools::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
   get "pages/dead_volume"
+  get "pages/calculate"
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
